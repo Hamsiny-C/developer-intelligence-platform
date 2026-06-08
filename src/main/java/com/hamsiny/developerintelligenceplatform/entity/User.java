756b0e142f3aaf8f22fa679b5bde88bb8f014a07
Hamsiny-C <hamsinyparimala2007@gmail.com>
2026-06-09 00:39:57 +0530

    Fix H2 user table issue

--- src/main/java/com/hamsiny/developerintelligenceplatform/entity/User.java
+++ src/main/java/com/hamsiny/developerintelligenceplatform/entity/User.java
@@ -4,23 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "users")
 @Data
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
+    private String name;
 
-private String name;
+    private String email;
 
-private String email;
+    private String githubUsername;
 
-private String githubUsername;
-
-private String password;
-
+    private String password;
 }
